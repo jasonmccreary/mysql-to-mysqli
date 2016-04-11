08cd5a6d081c347ee940a2d287ffb773df652783
PHP Shift <shift@php-shift.com>
2016-04-10 21:45:05 -0400

    Add required resource link for MySQLi functions

--- test.php
+++ test.php
@@ -1,10 +1,10 @@
 <?php
 $link      = mysql_connect('localhost', 'mysql_user', 'mysql_password');
-$thread_id = mysql_thread_id();
+$thread_id = mysqli_thread_id($mysqli_link, );
 
-mysql_select_db("mydb");
+mysqli_select_db($mysqli_link, "mydb");
 
-$result = mysql_query("SELECT id, name FROM mytable");
+$result = mysqli_query($mysqli_link, "SELECT id, name FROM mytable");
 
 while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
     printf("ID: %s  Name: %s", $row[0], $row[1]);
@@ -12,11 +12,11 @@ while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
 
 mysqli_free_result($result);
 
-mysql_query("SELECT id, name FROM mytable");
-mysql_query("SELECT id, name FROM mytable", $exisiting_link);
-mysql_real_escape_string('some string');
-mysql_real_escape_string('another string');
-mysql_real_escape_string('another string', $existing_link);
+mysqli_query($mysqli_link, "SELECT id, name FROM mytable");
+mysqli_query($exisiting_link, "SELECT id, name FROM mytable");
+mysqli_real_escape_string($mysqli_link, 'some string');
+mysqli_real_escape_string($mysqli_link, 'another string');
+mysqli_real_escape_string($existing_link, 'another string');
 
 mysql_list_tables('stuff');
 
