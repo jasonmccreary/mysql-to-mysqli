757b0157dd816545f2813b775ef6ad253ef93e51
Jason McCreary <jason@pureconcepts.net>
2016-04-10 20:57:14 -0400

    More refinements

--- test.php
+++ test.php
@@ -1,27 +1,28 @@
 <?php
 $link      = mysql_connect('localhost', 'mysql_user', 'mysql_password');
-$thread_id = mysql_thread_id($link);
+$thread_id = mysql_thread_id();
 
-mysql_select_db($mysql_link, "mydb");
+mysql_select_db("mydb");
 
-$result = mysql_query($mysql_link, "SELECT id, name FROM mytable");
+$result = mysql_query("SELECT id, name FROM mytable");
 
-while ($row = mysql_fetch_array($result, MYSQLI_NUM)) {
+while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
     printf("ID: %s  Name: %s", $row[0], $row[1]);
 }
 
 mysql_free_result($result);
 
-mysql_query($link, "SELECT id, name FROM mytable");
-mysql_real_escape_string($mysql_link, 'some string');
-mysql_real_escape_string($link, 'another string');
-mysql_real_escape_string($mysql_link, 'another string');
+mysql_query("SELECT id, name FROM mytable");
+mysql_query("SELECT id, name FROM mytable", $exisiting_link);
+mysql_real_escape_string('some string');
+mysql_real_escape_string('another string');
+mysql_real_escape_string('another string', $existing_link);
 
 mysql_list_tables('stuff');
 
 mysql_connect('localhost', 'mysql_user', 'mysql_password');
-mysql_connect('localhost', 'mysql_user', 'mysql_password');
-mysql_connect('localhost', 'mysql_user', 'mysql_password', 5, 1);
+mysql_connect('localhost', 'mysql_user', 'mysql_password', false);
+mysql_connect('localhost', 'mysql_user', 'mysql_password', true, 1);
 
 mysql_fetch_field($result);
 mysql_fetch_field($result, 5);
