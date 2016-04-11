fe1a8f3afc9c5184181f37f395935b341cf62ecc
Jason McCreary <jason@pureconcepts.net>
2016-04-10 20:40:23 -0400

    Sample test file

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+$link      = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+$thread_id = mysql_thread_id($link);
+
+mysql_select_db($mysql_link, "mydb");
+
+$result = mysql_query($mysql_link, "SELECT id, name FROM mytable");
+
+while ($row = mysql_fetch_array($result, MYSQLI_NUM)) {
+    printf("ID: %s  Name: %s", $row[0], $row[1]);
+}
+
+mysql_free_result($result);
+
+mysql_query($link, "SELECT id, name FROM mytable");
+mysql_real_escape_string($mysql_link, 'some string');
+mysql_real_escape_string($link, 'another string');
+mysql_real_escape_string($mysql_link, 'another string');
+
+mysql_list_tables('stuff');
+
+mysql_connect('localhost', 'mysql_user', 'mysql_password');
+mysql_connect('localhost', 'mysql_user', 'mysql_password');
+mysql_connect('localhost', 'mysql_user', 'mysql_password', 5, 1);
+
+mysql_fetch_field($result);
+mysql_fetch_field($result, 5);
